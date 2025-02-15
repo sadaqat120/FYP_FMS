ad120f440dd68e9f3a86aecebcb0a7a7a6013335
Sadaqat Rasool <sadaqatrasoolmsr@gmail.com>
2025-02-15 19:47:29 +0500

    Changed the folder structure and extract the learning files, removes the documentation files

--- frontend/src/components1/context/UserContext.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserContext = React.createContext("This is the default value");
-const UserProvider = UserContext.Provider;
-const UserConsumer = UserContext.Consumer;
-export { UserProvider, UserConsumer };
-export default UserContext
